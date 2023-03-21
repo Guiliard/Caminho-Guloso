00ebc55a061e14a73ec9c4bcdab382430a75cbbc
Rafael Moreira <127882640+Guiliard@users.noreply.github.com>
2023-03-21 17:43:37 -0300

    Add files via upload

---
+++ src/Matriz.hpp
@@ -0,0 +1,29 @@
+#ifndef __MATRIZ_HPP
+#define __MATRIZ_HPP
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+class Matriz
+{
+    private: 
+        short int tamanho;
+        short int total;
+        bool verificador;
+
+    public:
+    Matriz();
+    void setTamanho(short int tamanho);
+        short int getTamanho();
+    void setTotal(short int total);
+        short int getTotal();
+    void setVerificador(bool verificador);
+        bool getVerificador();
+    void Leitura(fstream& arq, short int **mat);
+    void Inicio(short int **mat);
+};
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include "Matriz.hpp"
+int main()
+{
+    Matriz *tabela = new Matriz();
+    short int tamanho;
+    string linha;
+    fstream arq;
+
+    arq.open("input.data");
+    if(arq.is_open())
+    {
+        arq >> tamanho;
+        getline(arq, linha);
+        short int **mat = new short int* [tamanho];
+        for(short int i=0; i<tamanho;i++)
+            mat[i]=new short int[tamanho];
+        tabela->setTamanho(tamanho);
+        while(!arq.eof())
+        {
+            tabela->Leitura(arq,mat);
+            if(tabela->getVerificador()==0)
+                tabela->Inicio(mat);
+        }
+    }   
+    arq.close();
+}
